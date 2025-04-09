bfcb94b4e391d5167d95c29b41f02c3169566999
Siva <siva9kolli@gmail.com>
2025-04-09 05:35:18 +0530

    design patterns

---
+++ src/test/java/DesignPatterns/FireFoxDriverManager.java
@@ -0,0 +1,11 @@
+package DesignPatterns;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxDriverManager implements BrowserDriver{
+    @Override
+    public WebDriver createDriver() {
+        return new FirefoxDriver();
+    }
+}
